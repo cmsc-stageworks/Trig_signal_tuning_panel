5d5030bf8cf800dc9fcc3f52994e368cab3ccbe2
RandomRobot6789 <jenkinsandrewp@gmail.com>
2026-05-28 18:52:22 -0600

    Update all files

---
+++ Sine_Wave/lib/MainBoard/MainBoard.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_BOARD_H
+#define MAIN_BOARD_H
+
+#include "MainBoardPinout.h"
+#include <TCA9555.h>
+
+int MainBoardStart(bool initSD = true);
+
+#endif
---
+++ Sine_Wave/src/trig.h
@@ -0,0 +1,10 @@
+#ifndef TRIG_H
+#define TRIG_H
+
+
+void init_trig();
+
+void loop_trig();
+
+
+#endif
