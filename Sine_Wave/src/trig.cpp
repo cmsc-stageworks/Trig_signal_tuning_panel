2ccbf553ce486173277b78c050cf9306f86bc745
RandomRobot6789 <jenkinsandrewp@gmail.com>
2026-05-28 19:38:44 -0600

    Simplify architecture of drawing functions

--- Sine_Wave/src/trig.cpp
+++ Sine_Wave/src/trig.cpp
@@ -12,7 +12,15 @@ static Adafruit_ILI9341 target_screen = Adafruit_ILI9341(&mainBoardSpi, MAIN_BOA
 static Adafruit_ILI9341 controlled_screen = Adafruit_ILI9341(&mainBoardSpi, MAIN_BOARD_LCD_DC, 
     MAIN_BOARD_LCD_2_CS, MAIN_BOARD_LCD_RESET);
 
-static waveform_puzzle current_waveform_puzzle;
+static waveform current_waveform_puzzle;
+static waveform current_user_attempt;
+static boolean has_current_puzzle;
+
+struct waveform {
+    float amplitude;
+    uint16_t frequency;
+    uint16_t phase_shift;
+};
 
 
 uint8_t read_knob(){
@@ -57,45 +65,13 @@ uint16_t denormalize(uint16_t y) { //y goes from -1 to 1
     return output;
 }
 
-void draw_trig(Adafruit_ILI9341 *screen, float (*trig_function)(float)){
+void draw_trig(Adafruit_ILI9341 *screen, waveform function_to_draw){
     screen->fillScreen(screen->color565(0,0,0));
     int h = screen->height();
     int w = screen->width();
     for(int x=0; x<w; x++){
-        screen->drawPixel(x, denormalize(trig_function(normalize(x))), screen->color565(74, 242, 98));
+        screen->drawPixel(x, denormalize(function_to_draw.amplitude * sin(function_to_draw.frequency * (function_to_draw.phase_shift + normalize(x)))), screen->color565(74, 242, 98));
     }
-}
-
-float target_trig_function(float x) {
-    //A goes from 0 to 1
-    float A = get_target_aplitude();
-    //k goes from 1 to 16
-    uint16_t k = get_target_frequency();
-    return A * sin(k * x);
-}
-
-float controlled_trig_function(float x) {
-    //A goes from 0 to 1
-    float A = get_user_amplitude();
-    //k goes from 1 to 16
-    uint16_t k = get_user_frequency();
-    return A * sin(k * x);
-}
-
-float get_user_amplitude() {
-    //read encoder
-}
-
-uint16_t get_user_frequency() {
-    //read encoder
-}
-
-float get_target_amplitude() {
-    //dunno how to do this
-}
-
-uint16_t get_target_frequency() {
-    //dunno how to do this
 }
 
 void init_trig(){
@@ -107,16 +83,17 @@ void init_trig(){
 
 void loop_trig(){
     //target_screen.fillScreen(target_screen.color565(255, 255, 255));
-    if (current_puzzle) { //TODO: Is this legal?
-        draw_trig(&target_screen, &example_trig_function);
-        draw_trig(&controlled_screen, &controlled_trig_function);
+    if (has_current_puzzle) {
+        draw_trig(&target_screen, current_waveform_puzzle);
+        draw_trig(&controlled_screen, current_user_attempt);
         check_puzzle_completion();
     }
 }
 
 void check_puzzle_completion() {
-    if ((get_user_amplitude() == get_target_amplitude()) && (get_user_frequency() == get_target_frequency())) {
-        //report puzzle completion
-        current_waveform_puzzle = None; //fix this
+    if ((current_user_attempt.amplitude == current_waveform_puzzle.amplitude) && (current_user_attempt.frequency == current_waveform_puzzle.frequency)) {
+        //doesn't check phase shift yet
+        has_current_puzzle = false;
+        //report puzzle completion somehow
     }
 }
