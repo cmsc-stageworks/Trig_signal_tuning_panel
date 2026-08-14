c80e0b0d482a18e6914bb17fba3c853ebfc2dec5
Swimnato <66497456+Swimnato@users.noreply.github.com>
2026-08-13 20:52:07 -0600

    added noise
    - added noise option
    - refactored rendering code

--- Sine_Wave/src/trig.cpp
+++ Sine_Wave/src/trig.cpp
@@ -7,6 +7,7 @@
 #include <Adafruit_seesaw.h>
 #include <Arduino.h>
 #include "global_flags.h"
+#include <esp_random.h>
 
 extern SPIClass mainBoardSpi;
 
@@ -27,6 +28,8 @@ float last_phase_shift;
 #define TRIG_SCREEN_WIDTH  480
 //should be the wavelength of a "period one" wave; all others are integer fractions of this
 
+static int8_t random_noise_buffer[TRIG_SCREEN_WIDTH]; //120
+
 float normalize(uint16_t x) {
     float MIN_NUM_HORIZ_PERIODS_IN_WIDTH = 0.8;
     uint16_t w = TRIG_SCREEN_WIDTH;
@@ -34,52 +37,55 @@ float normalize(uint16_t x) {
     return x_fractional * MIN_NUM_HORIZ_PERIODS_IN_WIDTH * 2 * PI;
 }
 
-uint16_t denormalize(float y) { //y goes from -1 to 1
+#define GET_RANDOM_NOISE(index, noise_level) ((noise_level) > 0 ? ((int32_t) (random_noise_buffer[(index)])) * ((int32_t) (noise_level)) / (127.0f) : 0.0f);
+
+float denormalize(float y) { //y goes from -1 to 1
     float MAX_AMPLITUDE_AS_FRACTION_OF_HEIGHT = 0.7;
-    uint16_t h = screen.height();
+    uint16_t h = screen.height(); //320
     float y_as_float = y * h * MAX_AMPLITUDE_AS_FRACTION_OF_HEIGHT / 2; //divide by 2 because the input has range 2
-    uint16_t output =  (h/2) + (float)y_as_float;
-    return output;
+    return y_as_float;
 }
 
 static const uint8_t frequencyConverter[16] = {0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20};
 
-void draw_trig(Adafruit_ST7796S *screen, waveform target, waveform attempt, waveform old_target, waveform old_attempt) {
+float calculate_y(waveform wave, uint16_t x_position, uint16_t phase_offset) {
+    return denormalize((wave.amplitude / 10.0f) * sin((2 * PI * ((float) wave.phase_shift) / 16.0f) + frequencyConverter[wave.frequency] * normalize(x_position + phase_offset)));
+}
+
+
+
+void draw_trig(Adafruit_ST7796S *screen, waveform target, waveform attempt, waveform old_attempt) {
+    esp_fill_random(random_noise_buffer, TRIG_SCREEN_WIDTH);
+    static int8_t old_target_line[TRIG_SCREEN_WIDTH];
     static uint16_t phase_offset = 0;
     #define phase_offset_delta SCANSPEED
     int h = screen->height();
     int w = screen->width();
     //draw waveforms
-    uint16_t old_last_target_y = 0;
     uint16_t old_last_attempt_y = 0;
     uint16_t last_target_y = 0;
     uint16_t last_attempt_y = 0;
-    uint16_t old_current_attempt_y = denormalize((old_attempt.amplitude / 10.0f) * sin((2 * PI * ((float) old_attempt.phase_shift) / 16.0f) + frequencyConverter[old_attempt.frequency] * normalize(phase_offset)));
-    uint16_t old_current_target_y = denormalize((old_target.amplitude / 10.0f) * sin((2 * PI * ((float) old_target.phase_shift) / 16.0f) + frequencyConverter[old_target.frequency] * normalize(phase_offset)));
-    uint16_t current_attempt_y = denormalize((attempt.amplitude / 10.0f) * sin((2 * PI * ((float) attempt.phase_shift) / 16.0f) + frequencyConverter[attempt.frequency] * normalize(phase_offset + phase_offset_delta)));
-    uint16_t current_target_y = denormalize((target.amplitude / 10.0f) * sin((2 * PI * ((float) target.phase_shift) / 16.0f) + frequencyConverter[target.frequency] * normalize(phase_offset + phase_offset_delta)));
-    screen->fillCircle(0, old_current_attempt_y, 2, 0);
-    screen->fillCircle(0, old_current_target_y, 2, 0);
-    screen->fillCircle(0, current_attempt_y, 2, screen->color565(242, 74, 98));
-    screen->fillCircle(0, current_target_y, 2, screen->color565(74, 242, 98));
+    uint16_t old_current_attempt_y = calculate_y(old_attempt, 0, phase_offset);
+    uint16_t current_attempt_y = calculate_y(attempt, 0, phase_offset + phase_offset_delta); 
+    uint16_t current_target_y = calculate_y(target, 0, phase_offset + phase_offset_delta) + GET_RANDOM_NOISE(0, target.noise_level);
     for(int x=1; x<w; x++){ //skip the first one because we just took care of it
         old_last_attempt_y = old_current_attempt_y;
         last_attempt_y = current_attempt_y;
-        old_current_attempt_y = denormalize((old_attempt.amplitude / 10.0f) * sin((2 * PI * ((float) old_attempt.phase_shift) / 16.0f) + frequencyConverter[old_attempt.frequency] * normalize(x + phase_offset)));
-        current_attempt_y = denormalize((attempt.amplitude / 10.0f) * sin((2 * PI * ((float) attempt.phase_shift) / 16.0f) + frequencyConverter[attempt.frequency] * normalize(x + phase_offset + phase_offset_delta)));
-        old_last_target_y = old_current_target_y;
         last_target_y = current_target_y;
-        old_current_target_y = denormalize((old_target.amplitude / 10.0f) * sin((2 * PI * ((float) old_target.phase_shift) / 16.0f) + frequencyConverter[old_target.frequency] * normalize(x + phase_offset)));
-        current_target_y = denormalize((target.amplitude / 10.0f) * sin((2 * PI * ((float) target.phase_shift) / 16.0f) + frequencyConverter[target.frequency] * normalize(x + phase_offset + phase_offset_delta)));
-        screen->drawLine(x-1, old_last_target_y, x, old_current_target_y, 0); //draw over the last line
+        old_current_attempt_y = calculate_y(old_attempt, x, phase_offset);
+        current_attempt_y = calculate_y(attempt, x, phase_offset + phase_offset_delta);
+        current_target_y = calculate_y(target, x, phase_offset + phase_offset_delta) + GET_RANDOM_NOISE(x, target.noise_level);
+        screen->drawLine(x-1, old_target_line[x-1] + (h / 2), x, old_target_line[x] + (h / 2), 0); //draw over the last line
         if ((((x + phase_offset)/(old_attempt.frequency >= 8 ? 1 : 3)) % 2) == 0) {
-            screen->drawLine(x-1, old_last_attempt_y, x, old_current_attempt_y, 0);
+            screen->drawLine(x-1, old_last_attempt_y + (h / 2), x, old_current_attempt_y + (h / 2), 0);
         }
-        screen->drawLine(x-1, last_target_y, x, current_target_y, screen->color565(74, 242, 98));
+        screen->drawLine(x-1, last_target_y + (h / 2), x, current_target_y + (h / 2), screen->color565(74, 242, 98));
         if ((((x + phase_offset + phase_offset_delta)/(attempt.frequency >= 8 ? 1 : 3)) % 2) == 0) {
-            screen->drawLine(x-1, last_attempt_y, x, current_attempt_y, screen->color565(242, 74, 98));
+            screen->drawLine(x-1, last_attempt_y + (h / 2), x, current_attempt_y + (h / 2), screen->color565(242, 74, 98));
         }
+        old_target_line[x-1] = last_target_y;
     }
+    old_target_line[w - 1] = current_target_y;
     phase_offset = (phase_offset + phase_offset_delta) % PHASE_OFFSET_PERIOD;
 }
 
@@ -93,11 +99,10 @@ void init_trig() {
 }
 
 void loop_trig() {
-    Serial.println("in trig loop");
     if (has_current_puzzle) { 
         //check_puzzle_update is now deprecated because the puzzle now updates every frame, due to scrolling
         static waveform old_target = target_puzzle, last_attempt = current_attempt;
-        draw_trig(&screen, target_puzzle, current_attempt, old_target, last_attempt);
+        draw_trig(&screen, target_puzzle, current_attempt, last_attempt);
         old_target = target_puzzle;
         last_attempt = current_attempt;
         check_puzzle_completion();
@@ -130,6 +135,7 @@ waveform create_random_puzzle() {
     puzzle.frequency = random(1, 16);
     puzzle.amplitude = random(1, 10); //changed min from 0 to 1
     puzzle.phase_shift = random(0, 15);
+    puzzle.noise_level = random(0, 12);
     return puzzle;
 }
 
--- Sine_Wave/src/waveform.h
+++ Sine_Wave/src/waveform.h
@@ -4,9 +4,10 @@
 #include <Arduino.h>
 
 struct waveform {
-    uint16_t frequency; //1-15
-    uint16_t amplitude; //1-10
-    uint16_t phase_shift; //0-15
+    uint8_t frequency; //1-15
+    uint8_t amplitude; //1-10
+    uint8_t phase_shift; //0-15
+    uint8_t noise_level; //0-8 corresponding to pixels of noise
 };
 
 #endif
